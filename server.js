b7f2b3518903258ccd8136f8e96e72db1aa905f0
Niksa Volarevic <niksa.volarevic@kompanion.se>
2018-12-18 10:57:13 +0100

    creating users model

--- server.js
+++ server.js
@@ -1,18 +1,33 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const Task = require('./api/models/todoListModel');
+const User = require('./api/models/userModel');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/Tododb');
+// mongoose.connect('mongodb://localhost/Tododb'); // local mongodb setup
+
+// mongodb atlas setup
+const uri = 'mongodb+srv://nikssa:%23N4poli%21@siterka-mongodb-wklcx.mongodb.net';
+const options = {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    dbName: 'users',
+    retryWrites: true
+};
+
+
+mongoose.connect(uri, options, function(error){
+    if (error) console.log('mongoose connection error: ' + error)
+});
+
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-const routes = require('./api/routes/todoListRoutes');
+const routes = require('./api/routes/usersRoutes');
 routes(app);
 
 app.listen(port);
